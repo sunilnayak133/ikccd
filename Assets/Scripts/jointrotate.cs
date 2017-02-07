e07fb8a2408835a8af92a4377dc4f14b250e4e96
Sunil Nayak <sunil.nayak133@gmail.com>
2017-02-07 17:17:15 -0500

    Add files via upload

---
+++ Assets/Scripts/jointrotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jointrotate : MonoBehaviour {
+
+    private float time = 0f;
+    private int intime = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (intime > time)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, time));
+        }
+
+        time += Time.deltaTime;
+        intime = ((int)time) + 1;
+
+	}
+}
