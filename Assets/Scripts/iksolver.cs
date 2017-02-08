c940d26ed858615463471612d315c6c872049003
sunilnayak133 <linus@Linuss-MacBook-Pro.local>
2017-02-07 22:12:59 -0500

    Fixed 3D
    3D is now working.

--- Assets/Scripts/iksolver.cs
+++ Assets/Scripts/iksolver.cs
@@ -41,10 +41,7 @@ public class iksolver : MonoBehaviour {
                 {
                     Vector3 r1 = joints[joints.Length - 1].transform.position - joints[i].transform.position;
                     Vector3 r2 = targ.transform.position - joints[i].transform.position;
-                    //if (r1.magnitude <= 0.001f)
-                    //    continue;
-                    //if (r2.magnitude <= 0.001f)
-                    //    continue;
+                   
                     if (r1.magnitude * r2.magnitude <= 0.001f)
                     {
                         cos[i] = 1;
@@ -53,24 +50,17 @@ public class iksolver : MonoBehaviour {
                     else
                     {
                         cos[i] = Vector3.Dot(r1, r2) / (r1.magnitude * r2.magnitude);
-                        if (Mathf.Abs(cos[i]) <= 0.0001f || Mathf.Abs(cos[i]) >= 0.9999f)
-                            continue;
+                        
                         sin[i] = (Vector3.Cross(r1, r2)).magnitude / (r1.magnitude * r2.magnitude);
-                        if (Mathf.Abs(sin[i]) <= 0.0001f || Mathf.Abs(sin[i]) >= 0.9999f)
-                            continue;
+                        
                     }
                     Vector3 axis = (Vector3.Cross(r1, r2)) / (r1.magnitude * r2.magnitude);
-                    axis = axis.normalized;
-                    //Debug.DrawRay(joints[i].transform.position, axis*5, Color.green,1);
-                    //Debug.DrawRay(joints[i].transform.position, r1*5, Color.blue,1);
-                    //Debug.DrawRay(joints[i].transform.position, r2*5, Color.red,1);
-
-                    //theta[i] = Mathf.Atan2(sin[i], cos[i]);
+                  
                     theta[i] = Mathf.Acos(Mathf.Max(-1, Mathf.Min(1, cos[i])));
                     if (sin[i] < 0.0f)
                         theta[i] = -theta[i];
                     theta[i] = (float)SimpleAngle(theta[i]) * Mathf.Rad2Deg;
-                    joints[i].transform.Rotate(axis, theta[i]);
+					joints [i].transform.Rotate (axis, theta [i], Space.World);
 
                 }
 
